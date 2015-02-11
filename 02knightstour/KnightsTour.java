cbc6d0d85e31f2b90f6b208db74973d44ed6452e
Yubin Kim <yubinkim57@gmail.com>
2015-02-11 13:06:27 -0500

    commit boolean solveDone

--- 02knightstour/KnightsTour.java
+++ 02knightstour/KnightsTour.java
@@ -55,5 +55,16 @@ public class KnightsTour{
 	return false;
     }
 
+    public boolean solveDone(){
+	for(int i=0;i<board.length;i++){
+	    for(int j=0; i<board[0].length; j++){
+		if(board[i][j]==0){
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
+
 
 }
