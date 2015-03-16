169f51c595950134425b097cba2bf9b763e3f031
Yubin <yubinkim57@gmail.com>
2015-03-16 13:06:51 -0400

    commit

--- 07MyLinkedList/LNode.java
+++ 07MyLinkedList/LNode.java
@@ -1,30 +1,30 @@
-public class LNode {
+public class LNode<T> {
 
-    private int value;
-    private LNode next;
+    private T value;
+    private LNode<T> next;
 
-    public LNode(int val){
+    public LNode<T> (T val){
 	setValue(val);
     }
 
-    public LNode(int val, LNode nex){
+    public LNode<T>(T val, LNode nex){
 	setValue(val);
 	setNext(nex);
     }
 
-    public void setValue(int val){
+    public void setValue(T val){
 	value = val;
     }
 
-    public int getValue(){
+    public T getValue(){
 	return value;
     }
 
-    public void setNext(LNode n){
+    public void setNext(LNode<T> n){
 	next = n;
     }
 
-    public LNode getNext(){
+    public LNode<T> getNext(){
 	return next;
     }
 
---
+++ 07MyLinkedList/LNode.java~
@@ -0,0 +1,37 @@
+public class LNode {
+
+    private int value;
+    private LNode next;
+
+    public LNode(int val){
+	setValue(val);
+    }
+
+    public LNode(int val, LNode nex){
+	setValue(val);
+	setNext(nex);
+    }
+
+    public void setValue(int val){
+	value = val;
+    }
+
+    public int getValue(){
+	return value;
+    }
+
+    public void setNext(LNode n){
+	next = n;
+    }
+
+    public LNode getNext(){
+	return next;
+    }
+
+    public String toString(){
+	return "" + value;
+
+    }
+
+
+}
