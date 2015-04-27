2ec872431295cd7cc648513cfd142e8c08ae447e
Yubin <yubinkim57@gmail.com>
2015-04-26 22:30:40 -0400

    treenode done

---
+++ 12Tree/TreeNode.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+
+public class TreeNode<T>{
+
+    private T data;
+    private TreeNode<T> left, right;
+
+
+    public TreeNode(T data){
+	setData(data);
+    }
+
+    public void setData(T value){
+	data = value;
+    }
+
+    public T getData(){
+	return data;
+    }
+
+    public TreeNode<T> getLeft(){
+	return left;
+    }
+
+    public void setLeft(TreeNode<T> lef){
+	left = lef;
+    }
+
+    public TreeNode<T> getRight(){
+	return right;
+    }
+
+    public void setRight(TreeNode<T> righ){
+	right = righ;
+    }
+
+
+}
