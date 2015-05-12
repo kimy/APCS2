f8bfc8d4793ef31233f20e3c3b8274400f509da9
Yubin <yubinkim57@gmail.com>
2015-05-12 12:50:44 -0400

    initial commit

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class MyHeap{
+
+    private int[] heap;
+    private boolean isMax;
+
+    public myHeap(){
+	isMax = true;
+    }
+
+    public myHeap(boolean max){
+	isMax = max;
+    }
+
+    public String toString(){
+
+	return "";
+    }
+
+    public int remove(){
+
+	return 0;
+
+    }
+
+    public void add(){
+
+    }
+
+    public void add(int n){
+
+    }
+
+    public int peek(){
+	return 0; 
+
+    }
+
+
+
+}
